4b2e9d4fb1c13af084ac414713899b30d4dd59a6
Krzysztof Antoszek <krzysztof@antoszek.net>
2019-02-10 18:12:39 +0100

    more stuff

---
+++ WASDMode/WASDMode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WASDMode.Properties;
+
+namespace WASDMode {
+  public class WASDMode : ApplicationContext {
+    private NotifyIcon trayIcon;
+
+    public WASDMode() {
+      trayIcon = new NotifyIcon() {
+        Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
+        ContextMenu = new ContextMenu(new MenuItem[] {
+          new MenuItem("Exit", this.Exit)
+        }),
+        Visible = true
+      };
+    }
+
+    void Exit(object sender, EventArgs e) {
+      trayIcon.Visible = false;
+      Application.Exit();
+    }
+  }
+}
