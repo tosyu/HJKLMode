2467836774461fd3b50d4e07f4778c1f29c25f15
Krzysztof Antoszek <krzysztof@antoszek.net>
2019-02-10 19:26:21 +0100

    working hjkl

--- HJKLMode/HJKLMode.cs
+++ HJKLMode/HJKLMode.cs
@@ -1,35 +1,76 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
+using SnagFree.TrayApp.Core;
 
 namespace HJKLMode
 {
   public class HJKLMode : ApplicationContext {
-    private NotifyIcon trayIcon;
-    private bool capturing;
+    private NotifyIcon _trayIcon;
+    private bool _capture = false;
+    private MenuItem _toggleMenu;
+    private GlobalKeyboardHook _hook;
 
     public HJKLMode() {
-      trayIcon = new NotifyIcon() {
+      _trayIcon = new NotifyIcon() {
         Icon = Icon.ExtractAssociatedIcon(Application.ExecutablePath),
         ContextMenu = new ContextMenu(new MenuItem[] {
-          new MenuItem("Enable", Enable),
-          new MenuItem("Disable", Disable),
+          _toggleMenu = new MenuItem("Enabled", Toggle),
           new MenuItem("Exit", Exit)
         }),
         Visible = true
       };
+
+      _hook = new GlobalKeyboardHook();
+      _hook.KeyboardPressed += OnKeyPressed;
     }
 
-    void Enable(object sender, EventArgs e) {
-      capturing = true;
+    ~HJKLMode() {
+      _hook.Dispose();
     }
 
-    void Disable(object sender, EventArgs e) {
-      capturing = false;
+    private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e) {
+      Debug.WriteLine(e.KeyboardData.VirtualCode);
+      if (e.KeyboardData.VirtualCode == GlobalKeyboardHook.H) {
+        if (e.KeyboardData.Flags == GlobalKeyboardHook.VkLwin) {
+          Toggle(sender, e);
+          e.Handled = true;
+        } else if (_capture) {
+          SendKeys.Send("{LEFT}");
+          e.Handled = true;
+        }
+      }
+
+      if (!_capture) {
+        return;
+      }
+
+      if (e.KeyboardData.VirtualCode == GlobalKeyboardHook.J) {
+        SendKeys.Send("{UP}");
+        e.Handled = true;
+      }
+
+      if (e.KeyboardData.VirtualCode == GlobalKeyboardHook.K) {
+        SendKeys.Send("{DOWN}");
+        e.Handled = true;
+      }
+
+      if (e.KeyboardData.VirtualCode == GlobalKeyboardHook.L) {
+        SendKeys.Send("{RIGHT}");
+        e.Handled = true;
+      }
+
+      return;
+    }
+
+    void Toggle(object sender, EventArgs e) {
+      Debug.WriteLine("toggle " + (!_capture));
+      _toggleMenu.Checked = _capture = !_capture;
     }
 
     void Exit(object sender, EventArgs e) {
-      trayIcon.Visible = false;
+      _trayIcon.Visible = false;
       Application.Exit();
     }
   }
