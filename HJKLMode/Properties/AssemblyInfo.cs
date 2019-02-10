fcd96945ed28b2cee7c8779fb171a5b40a2418b5
Krzysztof Antoszek <krzysztof@antoszek.net>
2019-02-10 20:02:04 +0100

    fix name

--- HJKLMode/Properties/AssemblyInfo.cs
+++ HJKLMode/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WASDMode")]
+[assembly: AssemblyTitle("HJKLMode")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WASDMode")]
+[assembly: AssemblyProduct("HJKLMode")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
